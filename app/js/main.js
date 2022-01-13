bb9411d36dd4f8a6b6bc32b463737fde9b789596
Krad24 <marhipov240692@gmail.com>
2022-01-14 00:10:49 +0200

    fix global flex + form-styler

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,16 @@
 $(function(){
 
+    // sidebar
+
     $('.info__btn').on('click', () => {
         $('.info__btn').toggleClass('info__btn--active');
         $('.info__items').toggleClass('info__items--active')
     })
+
+    //search form
+
+    $('.search__select').styler();
+
+
 
 })
