ab9e27f1f981175a57c2243a0344de2396da64a6
Krad24 <marhipov240692@gmail.com>
2022-02-11 14:53:07 +0200

    search extended

--- app/js/main.js
+++ app/js/main.js
@@ -169,8 +169,8 @@ $(function () {
         spacing: "10px",
         multiColor: {
             "startColor": "#FF0000", //RED
-            "endColor"  : "#00FF00"  //GREEN
-          },
+            "endColor": "#00FF00"  //GREEN
+        },
         starSvg: `<svg viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10.1594 5.02786L8.95106 1.30902C8.65171 0.38771 7.3483 0.387701 7.04894 1.30902L5.84062 5.02786H1.93039C0.961671 5.02786 0.558885 6.26748 1.3426 6.83688L4.50604 9.13525L3.29772 12.8541C2.99837 13.7754 4.05284 14.5415 4.83656 13.9721L8 11.6738L11.1634 13.9721C11.9472 14.5415 13.0016 13.7754 12.7023 12.8541L11.494 9.13525L14.6574 6.83688C15.4411 6.26748 15.0383 5.02786 14.0696 5.02786L10.1594 5.02786Z"/>
             </svg>`,
@@ -221,6 +221,68 @@ $(function () {
         $('.comments__item').removeClass('comments__item--active');
         $('.comments__item--speak').addClass('comments__item--active');
     })
+
+// search range
+    var $range = $(".js-range-slider"),
+        $inputFrom = $(".js-input-from"),
+        $inputTo = $(".js-input-to"),
+        instance,
+        min = 0,
+        max = 1000,
+        from = 0,
+        to = 0;
+
+    $range.ionRangeSlider({
+        skin: "round",
+        type: "double",
+        min: min,
+        max: max,
+        from: 200,
+        to: 800,
+        onStart: updateInputs,
+        onChange: updateInputs
+    });
+    instance = $range.data("ionRangeSlider");
+
+    function updateInputs(data) {
+        from = data.from;
+        to = data.to;
+
+        $inputFrom.prop("value", from);
+        $inputTo.prop("value", to);
+    }
+
+    $inputFrom.on("input", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < min) {
+            val = min;
+        } else if (val > to) {
+            val = to;
+        }
+
+        instance.update({
+            from: val
+        });
+    });
+
+    $inputTo.on("input", function () {
+        var val = $(this).prop("value");
+
+        // validate
+        if (val < from) {
+            val = from;
+        } else if (val > max) {
+            val = max;
+        }
+
+        instance.update({
+            to: val
+        });
+    });
+
+
 
     var mixer = mixitup('.catalog');
 })
--- gulpfile.js
+++ gulpfile.js
@@ -80,6 +80,7 @@ function scripts() {
             'node_modules/rateyo/src/jquery.rateyo.js',
             'node_modules/swiper/swiper-bundle.js',
             'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
